3e27dcd07615893b64ce76209b49d91594822ce3
Sravan Kumar <sravankumar.n92@outlook.com>
2018-09-17 20:08:50 -0600

    date methods

---
+++ date.js
@@ -0,0 +1,7 @@
+var d = new Date();
+console.log(d);
+
+var customDate = new Date(1992, 09, 12); //(yyyy, mm, dd, hh, mm, ss, milliss)
+console.log(customDate);
+console.log(customDate.toString()); // convert to string 
+
