850fd5be4724281bd070561bc2fdcda5190e362a
Sravan Kumar <sravankumar.n92@outlook.com>
2018-09-17 20:10:07 -0600

    math methods

---
+++ math.js
@@ -0,0 +1,11 @@
+console.log(Math.PI); 
+console.log(Math.round(10.4));
+console.log(Math.ceil(10.4));
+console.log(Math.floor(10.4));
+console.log(Math.pow(5,2));
+console.log(Math.sqrt(25)); 
+console.log(Math.min(1,2,3,4,5));
+console.log(Math.max(1,2,3,4,5));
+console.log(Math.random());
+console.log(Math.abs(-10.4));
+
