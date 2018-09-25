0dbd2c4795c441b5fef8a7999aa6bd0cdd035353
Savan Kumar <sravankumar.n92@outlook.com>
2018-09-25 00:41:38 -0400

    boolean and compare

---
+++ boolndcompare.js
@@ -0,0 +1,10 @@
+var x = "5"; 
+var y = 6; 
+console.log(x==5);
+console.log(x===5);
+console.log(x!=5);
+console.log(x!==5);
+console.log(x>=5);
+console.log(x<5);
+console.log(x>5 && y>5); //and 
+console.log(x>5||y>5); //or
