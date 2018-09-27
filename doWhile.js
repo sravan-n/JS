58c54fe828db8fe252811ade098bead32d92c9ca
Savan Kumar <sravankumar.n92@outlook.com>
2018-09-27 12:47:14 -0400

    do while loop

---
+++ doWhile.js
@@ -0,0 +1,14 @@
+var i =0;
+while (i<=10){
+    console.log(i);
+    i++;
+}
+
+var fruits = ['banana', 'kiwi', 'mango', 'apple', 'pineapple'];
+var x=0; 
+do {
+    console.log(fruits[x]);
+    x++; 
+}
+while (x<fruits.length);
+
