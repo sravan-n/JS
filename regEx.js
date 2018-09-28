2147fd160b19d1089f3c8ac66b8055f2b0ef711c
Savan Kumar <sravankumar.n92@outlook.com>
2018-09-28 00:48:30 -0400

    search and replace

---
+++ regEx.js
@@ -0,0 +1,7 @@
+var str = "This is Sravan Kumar";
+console.log(str.search("Sravan"));
+console.log(str.search(/sravan/i));
+var new_str = str.replace("Sravan", "Pranav");
+console.log(new_str);
+var new_str2 = str.replace(/sravan/i, "Arjun");
+console.log(new_str2);
