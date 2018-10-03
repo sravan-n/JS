8d0ad504516c588de0e7b87c01eaffac90acc314
Savan Kumar <sravankumar.n92@outlook.com>
2018-10-03 18:13:46 -0400

    arrow function

---
+++ arrow.js
@@ -0,0 +1,9 @@
+var a , b; 
+function myFun(a, b) {
+    return a+b; 
+}
+console.log(myFun(1,2));
+console.log("...............");
+var a, b; 
+const x = (a, b) => { return a + b};
+console.log(x(1,2));
