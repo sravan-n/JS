6d4c8038384d8b10cbcebfc93b27ed3e8bea2dac
Savan Kumar <sravankumar.n92@outlook.com>
2018-10-03 23:00:19 -0400

    clousers

---
+++ clousers.js
@@ -0,0 +1,7 @@
+function home(r1, r2) {
+    function myRoommates() {
+        return "My Roommates are " + r1 + " and " + r2 ;
+    }
+    return myRoommates();
+}
+console.log(home("sravan", "kumar"));
