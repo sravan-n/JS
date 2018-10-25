a1b137c5cb9636963c22daa08d227f729194f529
Savan Kumar <sravankumar.n92@outlook.com>
2018-10-25 14:51:13 -0400

    object declaration

--- object.js
+++ object.js
@@ -9,5 +9,14 @@ employee = new Object();
 employee.name = "sravan";
 employee.age = 26;
 
+// object as a function
+function human(name, age, country) {
+    this.name = name; 
+    this.age = age; 
+    this.country = country; 
+}
+var human1 = new human('sravan', 26, 'USA');
+
 console.log(person);
 console.log(employee);
+console.log(human1);
